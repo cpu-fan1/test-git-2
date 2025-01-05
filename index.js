ff88e2c9400afa8fcfadd44eb4c2e17f8c57a0ee
Maxim <some1.92@yandex.ru>
2025-01-05 14:20:12 +0300

    update

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 console.log('Hello World');
 
-<<<<<<< HEAD
 const userAge = 35;
 if (userAge >= 18) {
-=======
-const age = 20;
-if (age >= 18) {
->>>>>>> feature/user-access
     console.log('Вы совершеннолетний');
 } else {
     console.log('Вы несовершеннолетний');
